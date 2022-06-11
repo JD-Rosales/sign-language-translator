45c4592210959d3a6e0e0b0e2c09bc1e00b8bc8a
JD-Rosales <rosales.jake1999@gmail.com>
2022-06-11 16:12:52 +0800

    Added alphabet buttons in Practice page

--- src/pages/Practice.js
+++ src/pages/Practice.js
@@ -34,10 +34,42 @@ function Practice() {
         />
         </div>
 
-      <div className='asl-container'>
+        <div className='asl-container'>
 
+        </div>
       </div>
-        
+
+      <div className='text-container'>
+        <span>Click any letter to start</span>
+      </div>
+
+      <div className='btn-container'>
+        <button type="button">A</button>
+        <button type="button">B</button>
+        <button type="button">C</button>
+        <button type="button">D</button>
+        <button type="button">E</button>
+        <button type="button">F</button>
+        <button type="button">G</button>
+        <button type="button">H</button>
+        <button type="button">I</button>
+        <button type="button">G</button>
+        <button type="button">K</button>
+        <button type="button">L</button>
+        <button type="button">M</button>
+        <button type="button">N</button>
+        <button type="button">O</button>
+        <button type="button">P</button>
+        <button type="button">Q</button>
+        <button type="button">R</button>
+        <button type="button">S</button>
+        <button type="button">T</button>
+        <button type="button">U</button>
+        <button type="button">V</button>
+        <button type="button">W</button>
+        <button type="button">X</button>
+        <button type="button">Y</button>
+        <button type="button">Z</button>
       </div>
     </div>
   )
