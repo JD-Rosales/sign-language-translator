fb1bfd0a13214602ff852280a096ac27a3b3dbed
JD-Rosales <rosales.jake1999@gmail.com>
2022-06-14 21:29:56 +0800

    Added static design for SpellHandSign page

--- src/pages/FingerSpell.js
+++ src/pages/FingerSpell.js
@@ -3,7 +3,49 @@ import './FingerSpell.css'
 
 function FingerSpell() {
   return (
-    <div className='FingerSpell'>FingerSpell</div>
+    <div className='FingerSpell'>
+      <h2>FINGER SPELL THE WORD</h2>
+
+      <div className='top-controls'>
+
+        <div className='difficulty'>
+          <span>GAME LEVEL:</span>
+
+          <select>
+            <option value='easy'>EASY</option>
+            <option value='medium'>MEDIUM</option>
+            <option value='hard'>HARD</option>
+          </select>
+        </div>
+        
+        <div className='timer-container'>
+          <span>Time:</span>
+          <span>30:00.00</span>
+        </div>
+
+        <span>Gesture Confidence: 85.90%</span>
+      </div>
+
+      <div className='asl-container'>
+        
+      </div>
+
+      <div className='bottom-text'>
+        <span>Correct: 1</span>
+
+        <span>4/10</span>
+
+        <span>Wrong: 3</span>
+      </div>
+
+      <div className='bottom-controls'>
+        <button>START</button>
+
+        <span>FIRE</span>
+
+        <button>RESTART</button>
+      </div>
+    </div>
   )
 }
 
