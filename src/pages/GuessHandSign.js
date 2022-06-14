6969fe5871479b4e9d62df2eb46ff0ce42083e8d
JD-Rosales <rosales.jake1999@gmail.com>
2022-06-14 18:24:08 +0800

    Added static design for GuessHandSign page

--- src/pages/GuessHandSign.js
+++ src/pages/GuessHandSign.js
@@ -3,7 +3,44 @@ import './GuessHandSign.css'
 
 function GuessHandSign() {
   return (
-    <div className='GuessHandSign'>GuessHandSign</div>
+    <div className='GuessHandSign'>
+      <h2>GUESS THE HAND SIGNS</h2>
+
+      <div className='top-controls'>
+        <span>GAME LEVEL:</span>
+
+          <select>
+            <option value='easy'>EASY</option>
+            <option value='medium'>MEDIUM</option>
+            <option value='hard'>HARD</option>
+          </select>
+
+        <button>START</button>
+
+        <div className='timer-container'>
+          <span>Time:</span>
+          <span>30:00.00</span>
+        </div>
+      </div>
+
+      <div className='asl-img-container'>
+        
+      </div>
+
+      <div className='bottom-controls'>
+        <span>Correct: 1</span>
+
+        <span>4/10</span>
+
+        <span>Wrong: 3</span>
+      </div>
+
+      <div className='input-container'>
+        <input type="text" placeholder="Text here..."></input>
+
+        <button>SUBMIT</button>
+      </div>
+    </div>
   )
 }
 
